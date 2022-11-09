a786080ab4fbb4e9604e141c02905e552ada88bd
JeremyGamer13 <69337718+JeremyGamer13@users.noreply.github.com>
2022-11-08 19:36:43 -0700

    set song enums

--- enums.js
+++ enums.js
@@ -9,5 +9,57 @@ SMEnums.SongLineInfo.cream = [
     { start: 2634975, end: 2712150 },
     { start: 2723175, end: 2822400 }
 ]
+SMEnums.SongLineInfo.nami = [
+    { start: 115762, end: 264600 },
+    { start: 396900, end: 504393 },
+    { start: 512662, end: 595350 },
+    { start: 644962, end: 785531 },
+    { start: 909562, end: 1042360 },
+    { start: 1043212, end: 1124550 },
+    { start: 1174162, end: 1323000 },
+    { start: 1438762, end: 1587600 },
+    { start: 2232562, end: 2381400 },
+    { start: 2497162, end: 2646000 },
+    { start: 2761762, end: 2910600 },
+    { start: 3042900, end: 3158824 },
+    { start: 3160202, end: 3237215 }
+]
+SMEnums.SongLineInfo.stir = [
+    { start: 555660, end: 705600 },
+    { start: 837902, end: 987842 },
+    { start: 1120142, end: 1225982 },
+    { start: 1261262, end: 1367102 },
+    { start: 1684622, end: 1834562 },
+    { start: 1966862, end: 2046242 },
+    { start: 2107982, end: 2187362 },
+    { start: 2249102, end: 2385812 },
+    { start: 2390222, end: 2519582 },
+    { start: 2522522, end: 2660702 },
+    { start: 2663642, end: 2798882 },
+    { start: 2813582, end: 2892962 },
+    { start: 2954702, end: 3034082 },
+    { start: 3078182, end: 3166382 },
+    { start: 3289862, end: 3386882 },
+    { start: 3660302, end: 3810242 }
+]
+SMEnums.SongLineInfo.grease = [
+    { start: 1047375, end: 1146472 },
+    { start: 1223647, end: 1322872 },
+    { start: 1400047, end: 1499272 },
+    { start: 1576447, end: 1675672 },
+    { start: 1752847, end: 1928410 },
+    { start: 1929247, end: 2025660 },
+    { start: 2105647, end: 2160772 },
+    { start: 2182822, end: 2271022 },
+    { start: 2458447, end: 2645872 },
+    { start: 2811247, end: 2998672 },
+    { start: 3880672, end: 3968872 },
+    { start: 4046047, end: 4145272 },
+    { start: 4222447, end: 4321672 },
+    { start: 4398847, end: 4498072 },
+    { start: 4751647, end: 4850872 },
+    { start: 4928047, end: 5027272 },
+    { start: 5104447, end: 5203672 }
+]
 
 window.SMEnums = SMEnums
